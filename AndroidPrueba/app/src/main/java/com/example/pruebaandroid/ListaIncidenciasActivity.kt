edd9aabab307de110caf26b0e8d9d627d086ad35
RaulKas07 <raul.casasgomez@iesmiguelherrero.com>
2024-02-06 20:12:42 +0100

    Primer dia

---
+++ AndroidPrueba/app/src/main/java/com/example/pruebaandroid/ListaIncidenciasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pruebaandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaIncidenciasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_incidencias)
+    }
+}

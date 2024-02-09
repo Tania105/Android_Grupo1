fbff3649020525e2d311a535f5fa0b2ae261170c
Naego Sarophiel <45404677+naego-sarophiel@users.noreply.github.com>
2024-02-09 18:00:44 +0100

    Creado el modulo para notificaciones
    Creado el modulo simple para la utilizacion de las notificaciones y la peticion de permisos al usuario

--- ANDROID/Aplicacion/app/src/main/java/com/example/miincidencia/InicioActivity.kt
+++ ANDROID/Aplicacion/app/src/main/java/com/example/miincidencia/InicioActivity.kt
@@ -1,12 +1,64 @@
 package com.example.pruebaandroid
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+
 import com.example.miincidencia.R
 
 class InicioActivity : AppCompatActivity() {
+    companion object {
+        const val LOCATION_REQUEST_CODE =  1000 // Choose a unique number for your request code
+        const val CHANNEL_ID : String = "your_channel_id"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() {
+        val CHANNEL_NAME = "Your Channel Name"
+        val CHANNEL_DESCRIPTION = "Channel Description"
+
+
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
+                description = CHANNEL_DESCRIPTION
+            }
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+    }
+
+    fun sendNotification(context: Context, title: String, description: String) {
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // Replace with your notification icon
+            .setContentTitle(title)
+            .setContentText(description)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), LOCATION_REQUEST_CODE)
+        } else {
+            // Permission already granted, proceed with the operation
+            with(NotificationManagerCompat.from(context)) {
+                notify(System.currentTimeMillis().toInt(), builder.build())
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        // Call this method to create the notification channel
+        createNotificationChannel()
+        sendNotification(this, "Prueba", "Esto es una prueba")
     }
 }
